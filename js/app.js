d9ca8225ae1b18a65a206ea1a76a46764ece8325
Diego Frata <diego.frata@gmail.com>
2014-08-23 03:01:53 +0100

    Mais progresso.

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
 		var topbar = verge.rectangle($(".sticky"));
 		var sectionHeight = verge.viewportH() - topbar.height;
 		$('.full').css('min-height', sectionHeight);
+		$('#google-map').css('height', verge.rectangle($('#localizacao')).height);
 	};
 
 	var scrollHandler = function(){
@@ -49,6 +50,32 @@ $(document).ready(function(){
 		cssEase: 'linear',
 		arrows: false
 	});
+
+
+	function initialiseMaps() {
+	    var myLatLng = new google.maps.LatLng(-23.61132,-46.665704);
+	    var mapOptions = {
+	        zoom: 15,
+	        center: myLatLng,
+	        scrollwheel: false,
+	        disableDefaultUI: true,
+	        draggable: false,
+	        scaleControl: false,
+	        navigationControl: false,
+	        streetViewControl: false
+	    };
+
+	    var map = new google.maps.Map(document.getElementById('google-map'),
+	        mapOptions);
+
+	    google.maps.event.addListener(map, 'center_changed', function() {
+	        window.setTimeout(function() {
+	            map.panTo(marker.getPosition());
+	        }, 1500);
+	    });
+	};
+
+	google.maps.event.addDomListener(window, 'load', initialiseMaps);
 });
 
 
