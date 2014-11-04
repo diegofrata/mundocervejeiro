5644420849ac1d52be0a58a69ce05efdaf462da2
Diego Frata <diego.frata@gmail.com>
2014-11-04 23:52:53 +0000

    Added Google+ and the Brewery picture.

--- js/app.js
+++ js/app.js
@@ -147,6 +147,17 @@ $(document).ready(function(){
 		arrows: false
 	});
 
+	$('#fabricaslider').slick({
+		dots: false,
+		infinite: true,
+		speed: 1000,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		fade: true,
+		cssEase: 'linear',
+		arrows: false
+	});
+
 	$('#beerslider').slick({
 		dots: true,
 		infinite: true,
